c1f8fad8cefe42d9adf71bc1daaac46575cb6d3c
yuxin <yyang393@usc.edu>
2024-01-19 11:12:00 -0800

    bkup-0119

--- run_all.sh
+++ run_all.sh
@@ -12,6 +12,14 @@
 # 0115
 # bash run_parallel_sutlej.sh WIKI mixer_wiki_1l config
 
-bash run_parallel_sutlej.sh WIKI TGAT_1l_gru config
-bash run_parallel_sutlej.sh REDDIT TGAT_1l_gru config
-bash run_parallel_sutlej.sh REDDIT mixer_reddit_1l config
+# bash run_parallel_sutlej.sh WIKI TGAT_1l_gru config
+# bash run_parallel_sutlej.sh REDDIT TGAT_1l_gru config
+# bash run_parallel_sutlej.sh REDDIT mixer_reddit_1l config
+
+# 0116
+# bash run_parallel_sutlej.sh LASTFM TGAT_1l config
+# bash run_parallel_sutlej.sh MOOC TGAT_1l config
+
+# 0117
+bash run_parallel_sutlej.sh LASTFM mixer_1l config
+bash run_parallel_sutlej.sh MOOC mixer_1l config
--- run_parallel_sutlej.sh
+++ run_parallel_sutlej.sh
@@ -4,7 +4,7 @@
 # Run small datasets WIKI / REDDIT on ganges
 
 export CUDA_DEVICE_ORDER=PCI_BUS_ID;
-gpus=(2 3 4 5)
+gpus=(0 1 2 3)
 #echo "Enter a list of device numbers separated by commas:"
 #IFS=',' read -ra gpus
 #echo -n 'Schedule tasks on GPUs: '
@@ -16,7 +16,7 @@ gpus=(2 3 4 5)
 
 datasets=("${1}")
 orders=("chorno")
-runs=1
+runs=5
 
 trial="${2}"
 trial_dir="$(date +%Y-%m-%d)"_"${trial}"
