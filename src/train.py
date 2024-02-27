9c31c6b83a33d4b4f0c614ee4b6caf117d64641e
yuxin <yyang393@usc.edu>
2024-02-26 20:16:57 -0800

    fix a bug in training

--- src/train.py
+++ src/train.py
@@ -303,7 +303,7 @@ with torch.profiler.profile(
             globals.timer.print(prefix='\t')
             globals.timer.reset()
         no_improve += 1
-        if no_improve > 5 and e > 30:
+        if no_improve > early_stop and e > 30:
             break
 
 if args.tb_log_prefix != '':
