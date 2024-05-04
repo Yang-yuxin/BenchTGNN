4822a5e1c1ee5092b8735f9da7c9e78fd29cc856
yuxinyang <yyang393.usc>
2024-05-03 23:07:36 -0700

    clean up auxilliary files

--- process_benchtemp.sh
+++
@@ -1,9 +0,0 @@
-dataset="${1}"
-# cp DATA_BenchTemp/benchtemp_datasets/${dataset}/ml_${dataset}.csv DATA/${dataset}/edges.csv
-# python src/npy2pt.py --npy DATA_BenchTemp/benchtemp_datasets/${dataset}/ml_${dataset}_node.npy --pt DATA/${dataset}/node_features_pad.pt
-if [ ! -d "DATA/${dataset}" ]; then
-  mkdir DATA/${dataset}
-fi
-cp DATA_BenchTemp/benchtemp_datasets/${dataset}/ml_${dataset}.csv DATA/${dataset}/edges.csv &&  \
-python src/gen_graph.py --data ${dataset} && \
-python src/preprocess_benchtemp.py --data ${dataset}
